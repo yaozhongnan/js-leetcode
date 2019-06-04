3d5e2f01dc1f5ab5eba1f9301928e095f020afee
yzn <425923407@qq.com>
2019-06-04 18:15:08 +0800

    add twoSum and lengthOfLongestSubstring

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,20 @@ router.get('/', (req, res) => {
   res.render('index.html')
 })
 
+router.get('/_01', (req, res) => {
+  res.render('main/_01.html')
+})
+
+router.get('/_02', (req, res) => {
+  res.render('main/_02.html')
+})
+
 router.get('/001', (req, res) => {
   res.render('main/001.html')
 })
 
-router.get('/002', (req, res) => {
-  res.render('main/002.html')
+router.get('/003', (req, res) => {
+  res.render('main/003.html')
 })
 
 module.exports = router
